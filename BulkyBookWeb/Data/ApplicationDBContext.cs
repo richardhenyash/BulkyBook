71728a7b1f33c90eae718f44e71b20d7c93e482a
Richard Ash <richard.ash@solirius.com>
2022-12-22 15:30:37 +0000

    Initial commit

---
+++ BulkyBookWeb/Data/ApplicationDBContext.cs
@@ -0,0 +1,13 @@
+using BulkyBookWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyBookWeb.Data;
+
+public class ApplicationDBContext : DbContext
+{
+    public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+    {
+        
+    }
+    public DbSet<Category> Categories { get; set; }
+}
