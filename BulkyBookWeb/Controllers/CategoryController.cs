f4e60e868b531c6507dd52feb85c393d838a58ff
Richard Ash <richard.ash@solirius.com>
2022-12-29 11:10:54 +0000

    End of part 44

--- BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -1,6 +1,7 @@
 using BulkyBookWeb.Data;
 using BulkyBookWeb.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace BulkyBookWeb.Controllers;
 
@@ -40,6 +41,7 @@ public class CategoryController : Controller
         {
             _db.Categories.Add(obj);
             _db.SaveChanges();
+            TempData["success"] = "Category created successfully";
             return RedirectToAction("Index");
         }
 
@@ -71,11 +73,42 @@ public class CategoryController : Controller
         }
         if (ModelState.IsValid)
         {
-            _db.Categories.Add(obj);
+            _db.Categories.Update(obj);
             _db.SaveChanges();
+            TempData["success"] = "Category updated successfully";
             return RedirectToAction("Index");
         }
 
         return View(obj);
+    }
+    
+    //GET
+    public IActionResult Delete(int? id)
+    {
+        if (id == null || id == 0)
+        {
+            return NotFound();
+        }
+
+        var categoryFromDb = _db.Categories.Find(id);
+        // var categoryFromDbFirst = _db.Categories.FirstOrDefault(u=> u.Id == id);
+        // var categoryFromDbSingle = _db.Categories.SingleOrDefault(u=> u.Id == id);
+        return View(categoryFromDb);
+    }
+    
+    //POST
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult DeletePost(int? id)
+    {
+        var obj = _db.Categories.Find(id);
+        if (obj == null)
+        {
+            return NotFound();
+        }
+        _db.Categories.Remove(obj);
+        _db.SaveChanges();
+        TempData["success"] = "Category deleted successfully";
+        return RedirectToAction("Index");
     }
 }
