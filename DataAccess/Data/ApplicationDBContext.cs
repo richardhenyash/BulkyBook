f3de9c6ca71e1d598fcabf0c7478777ea7452bf3
Richard Ash <richard.ash@solirius.com>
2023-02-27 08:57:16 +0000

    End of part 80

---
+++ DataAccess/Data/ApplicationDBContext.cs
@@ -0,0 +1,13 @@
+using Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess;
+
+public class ApplicationDBContext : DbContext
+{
+    public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+    {
+        
+    }
+    public DbSet<Category> Categories { get; set; }
+}
