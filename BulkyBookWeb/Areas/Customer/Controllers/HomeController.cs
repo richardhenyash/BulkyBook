a31da8f38a13f8024c1500d8a16e8921bbec5180
Richard Ash <richard.ash@solirius.com>
2023-04-11 08:50:53 +0100

    End of part 332

--- BulkyBookWeb/Areas/Customer/Controllers/HomeController.cs
+++ BulkyBookWeb/Areas/Customer/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using System.Diagnostics;
+using BulkyBook.DataAccess.Repository.IRepository;
 using Microsoft.AspNetCore.Mvc;
 using Models;
 
@@ -7,15 +8,18 @@ namespace BulkyBookWeb.Controllers;
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly IUnitOfWork _unitOfWork;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
     {
         _logger = logger;
+        _unitOfWork = unitOfWork;
     }
 
     public IActionResult Index()
     {
-        return View();
+        IEnumerable<Product> productList = _unitOfWork.Product.GetAll(includeProperties: "Category,CoverType");
+        return View(productList);
     }
 
     public IActionResult Privacy()
