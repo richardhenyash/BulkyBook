19816d23c535c5dbbac6091ad42f4390eb8794f3
Richard Ash <richard.ash@solirius.com>
2023-04-13 09:08:31 +0100

    Upgrade to dotnet 7, scaffold identity. End of part 337

--- DataAccess/Data/ApplicationDbContext.cs
+++ DataAccess/Data/ApplicationDbContext.cs
@@ -1,9 +1,10 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace DataAccess;
 
-public class ApplicationDbContext : DbContext
+public class ApplicationDbContext : IdentityDbContext
 {
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
     {
