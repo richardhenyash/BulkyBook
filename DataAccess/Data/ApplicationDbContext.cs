9949c1f5c2d90a65a3e8dd3a2c054850e7352e62
Richard Ash <richard.ash@solirius.com>
2023-04-17 08:23:40 +0100

    End of part 343

--- DataAccess/Data/ApplicationDbContext.cs
+++ DataAccess/Data/ApplicationDbContext.cs
@@ -12,6 +12,7 @@ public class ApplicationDbContext : IdentityDbContext
     }
     public DbSet<Category> Categories { get; set; }
     public DbSet<CoverType> CoverTypes { get; set; }
-        
     public DbSet<Product> Products { get; set; }
+    
+    public DbSet<ApplicationUser> ApplicationUsers { get; set; }
 }
