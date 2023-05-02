b94866517e2c6b1748043c7cb22e545098f82cb2
Richard Ash <richard.ash@solirius.com>
2023-05-02 08:50:32 +0100

    End of part 358

--- DataAccess/Repository/IRepository/IShoppingCartRepository.cs
+++ DataAccess/Repository/IRepository/IShoppingCartRepository.cs
@@ -4,4 +4,6 @@ namespace BulkyBook.DataAccess.Repository.IRepository;
 
 public interface IShoppingCartRepository : IRepository<ShoppingCart>
 {
+    int IncrementCount(ShoppingCart shoppingCart, int count);
+    int DecrementCount(ShoppingCart shoppingCart, int count);
 }
--- DataAccess/Repository/ShoppingCartRepository.cs
+++ DataAccess/Repository/ShoppingCartRepository.cs
@@ -12,4 +12,14 @@ public class ShoppingCartRepository : Repository<ShoppingCart>, IShoppingCartRep
     {
         _db = db;
     }
+    public int DecrementCount(ShoppingCart shoppingCart, int count)
+    {
+        shoppingCart.Count -= count;
+        return shoppingCart.Count;
+    }
+    public int IncrementCount(ShoppingCart shoppingCart, int count)
+    {
+        shoppingCart.Count += count;
+        return shoppingCart.Count;
+    }
 }
