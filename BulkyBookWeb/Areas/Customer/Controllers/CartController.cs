766572eb1c61c1eb9df24677b62a4d196e9518d9
Richard Ash <richard.ash@solirius.com>
2023-05-05 09:06:41 +0100

    End of part 364

--- BulkyBookWeb/Areas/Customer/Controllers/CartController.cs
+++ BulkyBookWeb/Areas/Customer/Controllers/CartController.cs
@@ -37,6 +37,24 @@ public class CartController : Controller
         }
         return View(ShoppingCartVm);
     }
+    
+    public IActionResult Summary()
+    {
+        // var claimsIdentity = (ClaimsIdentity)User.Identity;
+        // var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+        // ShoppingCartVm = new ShoppingCartVm()
+        // {
+        //     ListCart = _unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == claim.Value,
+        //         includeProperties: "Product")
+        // };
+        // foreach (var cart in ShoppingCartVm.ListCart)
+        // {
+        //     cart.Price = getPriceBasedOnQuantity(cart.Count, cart.Product.Price, cart.Product.Price50,
+        //         cart.Product.Price100);
+        //     ShoppingCartVm.CartTotal += (cart.Price * cart.Count);
+        // }
+        return View();
+    }
 
     private double getPriceBasedOnQuantity(double quantity, double price, double price50, double price100)
     {
@@ -50,5 +68,34 @@ public class CartController : Controller
             return price50;
         }
         return price100;
+    }
+
+    public IActionResult Plus(int cartId)
+    {
+        var cart = _unitOfWork.ShoppingCart.GetFirstOrDefault(u => u.Id == cartId);
+        _unitOfWork.ShoppingCart.IncrementCount(cart, 1);
+        _unitOfWork.Save();
+        return RedirectToAction(nameof(Index));
+    }
+    public IActionResult Minus(int cartId)
+    {
+        var cart = _unitOfWork.ShoppingCart.GetFirstOrDefault(u => u.Id == cartId);
+        if (cart.Count <= 1)
+        { 
+            _unitOfWork.ShoppingCart.Remove(cart);
+        }
+        else
+        {
+            _unitOfWork.ShoppingCart.IncrementCount(cart, -1);
+        }
+        _unitOfWork.Save();
+        return RedirectToAction(nameof(Index));
+    }
+    public IActionResult Remove(int cartId)
+    {
+        var cart = _unitOfWork.ShoppingCart.GetFirstOrDefault(u => u.Id == cartId);
+        _unitOfWork.ShoppingCart.Remove(cart);
+        _unitOfWork.Save();
+        return RedirectToAction(nameof(Index));
     }
 }
