a4e79a87e9462f57c637cee39fb09a48b73addf3
Richard Ash <richard.ash@solirius.com>
2023-06-02 09:39:39 +0100

    End of part 384

--- BulkyBookWeb/Areas/Admin/Controllers/OrderController.cs
+++ BulkyBookWeb/Areas/Admin/Controllers/OrderController.cs
@@ -1,6 +1,7 @@
 using BulkyBook.DataAccess.Repository.IRepository;
 using Microsoft.AspNetCore.Mvc;
 using Models;
+using Utility;
 
 namespace BulkyBookWeb.Controllers;
 [Area("Admin")]
@@ -21,9 +22,26 @@ public class OrderController : Controller
     
     #region API CALLS
     [HttpGet]
-    public IActionResult GetAll()
+    public IActionResult GetAll(string status)
     {
         var orderHeaders = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser");
+        switch (status)
+        {
+            case "pending":
+                orderHeaders = orderHeaders.Where(u => u.PaymentStatus == StaticDetails.PaymentStatusDelayedPayment);
+                break;
+            case "inprocess":
+                orderHeaders = orderHeaders.Where(u => u.OrderStatus == StaticDetails.StatusInProgress);
+                break;
+            case "completed":
+                orderHeaders = orderHeaders.Where(u => u.OrderStatus == StaticDetails.StatusShipped);
+                break;
+            case "approved":
+                orderHeaders = orderHeaders.Where(u => u.OrderStatus == StaticDetails.StatusApproved);
+                break;
+            default:
+                break;
+        }
         return Json(new { data = orderHeaders });
     }
     #endregion
