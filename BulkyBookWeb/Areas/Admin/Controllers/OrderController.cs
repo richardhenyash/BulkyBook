865897a2cf74bd2bdda298db023b8d670168837a
Richard Ash <richard.ash@solirius.com>
2023-06-02 09:39:39 +0100

    End of part 385

--- BulkyBookWeb/Areas/Admin/Controllers/OrderController.cs
+++ BulkyBookWeb/Areas/Admin/Controllers/OrderController.cs
@@ -1,14 +1,20 @@
+using System.Security.Claims;
 using BulkyBook.DataAccess.Repository.IRepository;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Models;
+using Models.ViewModels;
+using Utility;
 
 namespace BulkyBookWeb.Controllers;
 [Area("Admin")]
+[Authorize]
 
 public class OrderController : Controller
 {
     private readonly IUnitOfWork _unitOfWork;
-
+    [BindProperty]
+    public OrderVm OrderVm { get; set; }
     public OrderController(IUnitOfWork unitOfWork)
     {
         _unitOfWork = unitOfWork;
@@ -19,11 +25,49 @@ public class OrderController : Controller
         return View();
     }
     
+    public IActionResult Details(int orderId)
+    {
+        OrderVm = new OrderVm
+        {
+            OrderHeader =
+                _unitOfWork.OrderHeader.GetFirstOrDefault(u => u.Id == orderId, includeProperties: "ApplicationUser"),
+            OrderDetail = _unitOfWork.OrderDetail.GetAll(u => u.OrderId == orderId, includeProperties: "Product"),
+        };
+        return View(OrderVm);
+    }
+    
     #region API CALLS
     [HttpGet]
-    public IActionResult GetAll()
+    public IActionResult GetAll(string status)
     {
-        var orderHeaders = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser");
+        IEnumerable<OrderHeader> orderHeaders;
+        if (User.IsInRole(StaticDetails.RoleAdmin) || User.IsInRole(StaticDetails.RoleEmployee))
+        {
+            orderHeaders = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser");
+        }
+        else
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+            orderHeaders = _unitOfWork.OrderHeader.GetAll(u => u.ApplicationUserId == claim.Value, includeProperties: "ApplicationUser");
+        }
+        switch (status)
+        {
+            case "pending":
+                orderHeaders = orderHeaders.Where(u => u.PaymentStatus == StaticDetails.PaymentStatusDelayedPayment);
+                break;
+            case "inprocess":
+                orderHeaders = orderHeaders.Where(u => u.OrderStatus == StaticDetails.StatusInProgress);
+                break;
+            case "completed":
+                orderHeaders = orderHeaders.Where(u => u.OrderStatus == StaticDetails.StatusShipped);
+                break;
+            case "approved":
+                orderHeaders = orderHeaders.Where(u => u.OrderStatus == StaticDetails.StatusApproved);
+                break;
+            default:
+                break;
+        }
         return Json(new { data = orderHeaders });
     }
     #endregion
--- BulkyBookWeb/wwwroot/js/order.js
+++ BulkyBookWeb/wwwroot/js/order.js
@@ -1,13 +1,34 @@
 var dataTable;
 
 $(document).ready(function () {
-  loadDataTable();
+  var url = window.location.search;
+  if (url.includes("inprocess")) {
+    loadDataTable("inprocess");
+  }
+  else {
+    if (url.includes("completed")) {
+      loadDataTable("completed");
+    }
+    else {
+      if (url.includes("pending")) {
+        loadDataTable("pending");
+      }
+      else {
+        if (url.includes("approved")) {
+          loadDataTable("approved");
+        }
+        else {
+          loadDataTable("all");
+        }
+      }
+    }
+  }
 });
 
-function loadDataTable() {
+function loadDataTable(status) {
   dataTable = $('#tblData').DataTable({
     "ajax": {
-      "url":"/Admin/Order/GetAll"
+      "url": "/Admin/Order/GetAll?status=" + status
     },
     "columns": [
       { "data": "id", "width": "5%" },
@@ -20,16 +41,15 @@ function loadDataTable() {
         "data": "id",
         "render": function (data) {
           return `
-            <div class="w-75 btn-group" role="group">
-                <a href="/Admin/Order/Details?orderId=${data}"
-                    class="btn btn-primary mx-2">
-                    <i class="bi bi-pencil-square"></i>
-                </a>
-            </div>
-            `
+                    <div class="w-75 btn-group" role="group">
+                        <a href="/Admin/Order/Details?orderId=${data}" class="btn btn-primary mx-2">
+                            <i class="bi bi-pencil-square"></i>
+                        </a>
+					          </div>
+                 `
         },
         "width": "5%"
-      },
+      }
     ]
   });
 }
