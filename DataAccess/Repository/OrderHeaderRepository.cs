585f31f84472f85d5f6b07b572469684c6d2aa46
Richard Ash <richard.ash@solirius.com>
2023-06-06 09:40:36 +0100

    End of part 386

--- DataAccess/Repository/OrderHeaderRepository.cs
+++ DataAccess/Repository/OrderHeaderRepository.cs
@@ -29,6 +29,7 @@ public class OrderHeaderRepository : Repository<OrderHeader>, IOrderHeaderReposi
     public void UpdateStripePaymentId(int id, string sessionId, string? paymentIntentId)
     {
         var orderFromDb = _db.OrderHeaders.FirstOrDefault(u => u.Id == id);
+        orderFromDb.PaymentDate = DateTime.Now;
         orderFromDb.SessionId = sessionId;
         orderFromDb.PaymentIntentId = paymentIntentId;
     }
